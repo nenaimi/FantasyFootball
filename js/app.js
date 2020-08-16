6a1e78776da7a82502969e63a43897e5186f1347
Nick Mallare <nicholasmallare@gmail.com>
2020-08-16 14:58:48 +0200

    Add Vendor Folder

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+
 //Checking to see if our browser supports service workers
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('./sw.js')
         .then((reg) => console.log('Service worker registered', reg))
         .catch((err) => console.log('Service worker not registered', err))
 }
+
+
